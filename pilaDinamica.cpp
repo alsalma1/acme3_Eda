40d74b586eb08ac46ae8e6bbabf3afc4e50dc589
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2026-09-25 15:52:50 +0000

    falta main

--- pilaDinamica.cpp
+++ pilaDinamica.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
 
-#include "estructuraDinamica.h"
+#include "pilaDinamica.h"
 
 using namespace std;
 
 
 pilaDinamica::pilaDinamica(){
-  
+    inici = nullptr;
+}
+
+pilaDinamica::~pilaDinamica(){
+    while (!Buida()) {
+        Desempila();
+    }
+}
+
+bool pilaDinamica::Buida() const{
+    bool esBuida = false;
+    if(inici == nullptr) esBuida = true;
+
+    return esBuida;
+}
+
+void pilaDinamica::Empila(element e) {
+    node *p = new node;
+    p->dada = e;
+    if (Buida()) {
+        inici = p;
+        p->seguent = nullptr;
+    }
+    else {
+        p->seguent = inici;
+        inici = p;
+    }
+}
+
+void pilaDinamica::Desempila(){
+    if (!Buida()) {
+        node *p = inici;
+        inici = p->seguent;
+        delete p;
+    }
+
+}
+
+element pilaDinamica::Cim() const {
+    element cim;
+
+    if (Buida())
+        cim = '*';
+    else
+        cim = inici->dada;
+
+    return cim;
 }
